bc68f612acc5d42fd24fc58a50b018ddbbb32583
Elijah Smith <elijahsmith@Interapts-MacBook-Pro-71.local>
2018-02-13 15:31:52 -0500

    Added prints and lists

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -1,19 +1,63 @@
 package com.company;
 
-public class Library extends Game {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
-    public Library(String title) {
-        super(title);
+public class Library  {
+
+
+    public final Menu menu;
+    public  List<Game> gamelibrary = new ArrayList<Game>();
+    private List<Game> checkedOutGames = new ArrayList<Game>();
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
+
+
+    public Library(Menu menu) {
+        this.menu = menu;
     }
 
-    protected void addGame() {
+    protected void addGame(Game game) {
         // code goes here to add game to where ever we are savaing game things to
         //Save game title
         //add name to a view list
-        
+        gamelibrary.add(game);
+        menu.startMenu();
 
 
-        System.out.println(getTitle() + " has been to your library");
+    }
+
+    protected void removeGame(int gameIndex) {
+
+        gameIndex -= gameIndex;
+        gamelibrary.remove(gameIndex);
+        System.out.println("This game has been removed from your library");
+        menu.startMenu();
+
+    }
+    protected void checkoutGame(int gameIndex) {
+        gameIndex -= gameIndex;
+
+        Game game = gamelibrary.get(gameIndex);
+
+        checkedOutGames.add(game);
+        Calendar calendar = Calendar.getInstance();
+        System.out.println("You checked this game out on: " + dateFormat.format(calendar.getTime()));
+        calendar.add(Calendar.DAY_OF_YEAR,3);
+        System.out.println("This game is due back on: " + dateFormat.format(calendar.getTime()));
+        game.setDueDate(dateFormat.format(calendar.getTime()));
+        gamelibrary.remove(gameIndex);
+        menu.startMenu();
+
+    }
+    protected void checkInGame(int gameIndex) {
+
+        gameIndex -= gameIndex;
+
+        Game game = gamelibrary.get(gameIndex);
+
+        checkedOutGames.remove(game);
 
 
     }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -7,6 +7,7 @@ public class Menu {
     //User Menu to prompt user for library options
 
     private Scanner input = new Scanner(System.in);
+    private Library library = new Library(this);
     public void startMenu() {
 
         System.out.println("Welcome to the video game library. What would you like to do? \n" +
@@ -23,17 +24,27 @@ public class Menu {
             switch (input.nextInt()) {
                 case 1:
                     //Add game
-                    System.out.println("What is the name of the game you would like to add?");
-
+                    System.out.println("You have chosen to add a game. \n" + "What is the name of the game you would like to add?");
+                    input.nextLine();
+                    Game game = new Game(input.nextLine());
+                    System.out.println("You have added " + game.getTitle() + " to your library, this game can now be checked out.");
+                    library.addGame(game);
                     break;
                 case 2:
                     //Remove game
+                    System.out.println("You have chosen to remove a game from your library. \n" + "Here os a list of games, type the number of the one you would like to remove");
+                    // create method to list all games availible
+                    library.removeGame(input.nextInt());
                     break;
                 case 3:
                     //View library
+                    System.out.println(library.gamelibrary);
+                    startMenu();
                     break;
                 case 4:
                     //Check out
+                    System.out.println("Which game would you like to remove? Type in its number");
+                    library.checkoutGame(input.nextInt());
                     break;
                 case 5:
                     //check in
